a6160a31b7f00580c05903475833e4f2b2d5057e
Jake Topping <email@jaketopping.co.uk>
2021-05-16 23:12:40 +0100

    Ready for hyperparameter tuning

--- hyper_tuning/run_qm9_benchs_tuning.py
+++ hyper_tuning/run_qm9_benchs_tuning.py
@@ -16,18 +16,33 @@ import os
 import re
 import subprocess
 
+uma_args = {
+    'curvature_fn':['uma_forman'],
+    'consider_positivity':[True, False],
+    'target_curvature':[-1,0,1],
+    'scaling':[1,2,5,10],
+    'max_steps':[2,3,5,10]
+}
+unbiased_args = {
+    'curvature_fn':['unbiased_forman'],
+    'consider_positivity':[True, False],
+    'target_curvature':[-0.5,0,0.5],
+    'scaling':[10,25,100],
+    'max_steps':[2,3,5,10]
+}
+ARGUMENTS = list(product(*list(uma_args.values()))) + list(product(*list(unbiased_args.values())))
+
 
 MODEL_TYPES = ["GGNN", "RGCN", "RGAT", "RGIN"]#, "GNN-Edge-MLP0", "GNN-Edge-MLP1", "GNN_FiLM"]
 TASKS = ["mu", "alpha", "HOMO", "LUMO", "gap", "R2", "ZPVE", "U0", "U", "H", "G", "Cv", "Omega"]
-USE_SDRF = [True, False]
 
 pbs_array_index = int(os.environ['PBS_ARRAY_INDEX'])
-model_, task_, use_sdrf_ = list(product(MODEL_TYPES, TASKS, USE_SDRF))[pbs_array_index]
-_, task_id_, _ = list(product(MODEL_TYPES, range(len(TASKS)), USE_SDRF))[pbs_array_index]
+model_, task_, arguments_ = list(product(MODEL_TYPES, TASKS, ARGUMENTS))[pbs_array_index]
+_, task_id_, _ = list(product(MODEL_TYPES, range(len(TASKS)), ARGUMENTS))[pbs_array_index]
 MODEL_TYPES = [model_]
 TASKS = [task_]
 TASK_IDS = [task_id_]
-USE_SDRF = [use_sdrf_]
+ARGUMENTS = [arguments_]
 
 TEST_RES_RE = re.compile('^Metrics: MAEs: \d+:([0-9.]+) \| Error Ratios: \d+:([0-9.]+)')
 TIME_RE = re.compile('^Training took (\d+)s')
@@ -39,23 +54,27 @@ def run(args):
     print("Starting QM9 experiments, will write logfiles for runs into %s." % target_dir)
     num_seeds = int(args.get('--num-runs'))
     results = {}
-    for use_sdrf in USE_SDRF:
-        results[use_sdrf] = {}
+    for arguments in ARGUMENTS:
+        arguments_str = ';'.join(['f{x}={y}' for x,y in zip(list(uma_args.keys()), arguments)])
+        curvature_fn, consider_positivity, target_curvature, scaling, max_steps = arguments
+        results[arguments_str] = {}
         for model in MODEL_TYPES:
-            results[use_sdrf][model] = [{"test_errors": [], "times": []} for _ in TASKS]
+            results[arguments_str][model] = [{"test_errors": [], "times": []} for _ in TASKS]
             for task_id in TASK_IDS:
                 for seed in range(1, 1 + num_seeds):
-                    logfile = os.path.join(target_dir, "%s_task%i_seed%i_sdrf%s.txt" % (model, task_id, seed, use_sdrf))
+                    logfile = os.path.join(target_dir, "%s_task%i_seed%i_%s.txt" % (model, task_id, seed, arguments_str))
                     with open(logfile, "w") as log_fh:
                         subprocess.check_call(["python",
-                                            "train.py",
+                                            "../train.py",
                                             "--run-test",
                                             model,
                                             "QM9",
                                             "--model-param-overrides",
                                             '{\"random_seed\": %i}' % seed,
                                             "--task-param-overrides",
-                                            '{\"task_ids\": [%i], \"preprocess_with_sdrf\": \"%s\"}' % (task_id, use_sdrf),
+                                            '''{\"task_ids\": [%i], \"preprocess_with_sdrf\": \"True\", \"sdrf_curvature_fn\": \"%s\",
+\"sdrf_consider_positivity\": \"%s\", \"sdrf_target_curvature\": %f, \"sdrf_scaling\": %i, \"sdrf_max_steps\": %i}''' % \
+(task_id, curvature_fn, consider_positivity, target_curvature, scaling, max_steps),
                                             ],
                                             stdout=log_fh,
                                             stderr=log_fh)
@@ -64,20 +83,21 @@ def run(args):
                             time_match = TIME_RE.search(line)
                             res_match = TEST_RES_RE.search(line)
                             if time_match is not None:
-                                results[use_sdrf][model][0]["times"].append(int(time_match.groups()[0]))
+                                results[arguments_str][model][0]["times"].append(int(time_match.groups()[0]))
                             elif res_match is not None:
-                                results[use_sdrf][model][0]["test_errors"].append(float(res_match.groups()[1]))
+                                results[arguments_str][model][0]["test_errors"].append(float(res_match.groups()[1]))
 
     row_fmt_string = "%7s " + "&% 35s " * len(MODEL_TYPES) + "\\\\"
     print(row_fmt_string % tuple([""] + MODEL_TYPES))
     for task_id, task in enumerate(TASKS):
         task_id = 0 #task_id_
         model_results = []
-        for use_sdrf in USE_SDRF:
+        for arguments in ARGUMENTS:
+            arguments_str = ';'.join(['f{x}={y}' for x,y in zip(list(uma_args.keys()), arguments)])
             for model in MODEL_TYPES:
-                err = np.mean(results[use_sdrf][model][task_id]["test_errors"])
-                std = np.std(results[use_sdrf][model][task_id]["test_errors"])
-                time_in_min = np.mean(results[use_sdrf][model][task_id]["times"]) / 60
+                err = np.mean(results[arguments_str][model][task_id]["test_errors"])
+                std = np.std(results[arguments_str][model][task_id]["test_errors"])
+                time_in_min = np.mean(results[arguments_str][model][task_id]["times"]) / 60
                 model_results.append("%.2f & ($\pm %.2f$; $%.1f$min)" % (err, std, time_in_min))
         print(row_fmt_string % tuple([task] + model_results))
 
--- tasks/sdrf.py
+++ tasks/sdrf.py
@@ -3,7 +3,7 @@ import numpy as np
 
 softmax = lambda x: np.exp(x)/sum(np.exp(x))
 
-def uma_forman_parallel(A, N, return_extra=True):
+def uma_forman_parallel(G, A, N, target_curvature, return_extra=True):
     NC2 = int(N*(N-1)/2)+1
     E = np.zeros((NC2,N,N))
     no_of_edges = np.zeros(NC2)
@@ -27,15 +27,107 @@ def uma_forman_parallel(A, N, return_extra=True):
     degrees_reshaped = degrees.reshape((NC2,-1,N))
     degrees_reshaped_T = degrees.reshape((NC2,N,-1))
     squares = np.matmul(AE2,AE) - degrees_reshaped - degrees_reshaped_T + 1
-    
+
+    curvatures = (4 - degrees_reshaped - degrees_reshaped_T + 3*AE2 + squares)*AE
+
     if return_extra:
-        return (4 - degrees_reshaped - degrees_reshaped_T + 3*AE2 + squares)*AE, AE, no_of_edges, mapping
-    return (4 - degrees_reshaped - degrees_reshaped_T + AE2 + squares)*A
+        C = curvatures[0]
+        minima = np.argwhere(C == np.min(C))
+        ijs = [(mini[0],mini[1]) for mini in minima]
+        return curvatures, AE, no_of_edges, mapping, ijs
+    return curvatures
+
+
+def slanted_adj(A):
+    b = np.zeros((A.shape[0], A.shape[1]*A.shape[1]))
+    b[:, ::A.shape[1]+1] = A
+    return np.swapaxes(b.reshape(A.shape[0], A.shape[1], A.shape[1]), 0, 1)
+
+def slanted_adj_4D(A):
+    b = np.zeros((A.shape[0], A.shape[1], A.shape[2]*A.shape[2]))
+    b[:, :, ::A.shape[2]+1] = A
+    return np.swapaxes(b.reshape(A.shape[0], A.shape[1], A.shape[2], A.shape[2]), 1, 2)
+
+def linalg_way_lambda(A, N):
+    Ax = A.reshape(N,N,-1)
+    Ay = A.reshape(N,-1,N)
+    T = (A.dot(A * (1 - Ax)) * (1 - slanted_adj(A))) - 1
+    four_cycles = T * Ax * (1 - Ay)
+    sharps = (four_cycles > 0).sum(axis=0)
+    lambdas = four_cycles.max(axis=0)
+    lambdas = np.maximum(lambdas, lambdas.T)
+    return sharps, lambdas
+
+def linalg_way_lambda_3D(AE, N):
+    M = len(AE)
+    Ax = AE.reshape(M,N,N,-1)
+    Ay = AE.reshape(M,N,-1,N)
+    T = (np.einsum('...ij,...kjm->...ikm', AE, np.einsum('...jk,...ijk->...ijk', AE, (1-Ax))) * (1 - slanted_adj_4D(AE))) - 1
+    four_cycles = T * Ax * (1 - Ay)
+    sharps = (four_cycles > 0).sum(axis=1)
+    lambdas = four_cycles.max(axis=1)
+    lambdas = np.maximum(lambdas, np.einsum('ijk->ikj', lambdas))
+    return sharps, lambdas
+
+def unbiased_forman(A, N):
+    degrees = A.sum(axis=0)
+    degrees_reshaped = degrees.reshape((-1,N))
+    degrees_reshaped_T = degrees.reshape((N,-1))
+    degrees_max = np.maximum(degrees_reshaped, degrees_reshaped_T)
+    degrees_min = np.maximum(degrees_reshaped_T, degrees_reshaped)
+    A2 = np.matmul(A,A)
+    sharps, lambdas = linalg_way_lambda(A,N)
+    return (np.round((2 / degrees_reshaped) + (2 / degrees_reshaped_T) - 2 + 2*A2/degrees_max + A2/degrees_min \
+        + np.nan_to_num((sharps + sharps.T)/(lambdas*degrees_max)),2)) * A
+
+def unbiased_forman_focused(G, A, N, target_curvature, return_extra=True):
+    base_no_of_edges = A.sum()
+    no_of_edges = np.array([base_no_of_edges])
+    
+    C = unbiased_forman(A,N)
+    minC = np.min(C)
+    if minC >= target_curvature:
+        if return_extra:
+            return (C*A).reshape(1,N,N), A.reshape(1,N,N), no_of_edges, {}, []
+        else:
+            return (C*A).reshape(1,N,N)
+    
+    minima = np.argwhere(C == minC)
+    ijs = [(minimum[0],minimum[1]) for minimum in minima]
+    ijs = [(i,j) for (i,j) in ijs if i < j]
+    E = np.zeros((1,N,N))
+    l = 1
+    mapping = {}
+    
+    for i, j in ijs:
+        for x in list(G.neighbors(i)) + [i]:
+            for y in list(G.neighbors(j)) + [j]:
+                x_, y_ = (x,y) if x<y else (y,x)
+                if x_ != y_ and not A[x_,y_] and not (x_,y_) in mapping:
+                    E = np.append(E, np.zeros((1,N,N)), axis=0)
+                    E[l,x,y] = E[l,y,x] = 1
+                    no_of_edges = np.append(no_of_edges, [base_no_of_edges+1])
+                    mapping[(x_,y_)] = l
+                    l += 1
+    NC2 = l
+    
+    AE = A+E
+    AE2 = np.matmul(AE,AE)
+    degrees = np.diagonal(AE2, axis1=1, axis2=2)
+    degrees_reshaped = degrees.reshape((NC2,-1,N))
+    degrees_reshaped_T = degrees.reshape((NC2,N,-1))
+    degrees_max = np.maximum(degrees_reshaped, degrees_reshaped_T)
+    degrees_min = np.minimum(degrees_reshaped, degrees_reshaped_T)
+    sharps, lambdas = linalg_way_lambda_3D(AE,N)
+    curvatures = (2 / degrees_reshaped) + (2 / degrees_reshaped_T) - 2 + 2*AE2/degrees_max + AE2/degrees_min + np.nan_to_num((sharps + np.einsum('ijk->ikj', sharps))/(lambdas*degrees_max))
+    if return_extra:
+        return curvatures*AE, AE, no_of_edges, mapping, ijs
+    return curvatures*AE
 
 
 def focused_stochastic_ricci_step(G, A, N, curvature_fn, target_curvature=0, give_graphs_ricci_attribute=True,
                           prioritise_betweenness=False, consider_positivity=True):
-    curvatures, AE, no_of_edges, mapping = curvature_fn(A, N)
+    curvatures, AE, no_of_edges, mapping, ijs = curvature_fn(G, A, N, target_curvature)
     
     candidates = {}
     x = AE*target_curvature - curvatures
@@ -44,10 +136,6 @@ def focused_stochastic_ricci_step(G, A, N, curvature_fn, target_curvature=0, giv
     mses = (x**2).sum(axis=2).sum(axis=1)/no_of_edges
     mses = mses[0] - mses
     
-    C = curvatures[0]
-    minima = np.argwhere(C == np.min(C))
-    ijs = [(mini[0],mini[1]) for mini in minima]
-    
     if prioritise_betweenness:
         betweenness = {k:v for (k,v) in nx.algorithms.centrality.edge_betweenness_centrality(G).items() if k in ijs}
         betweenness = {k: v for k, v in sorted(betweenness.items(), key=lambda item: -item[1])}
@@ -55,22 +143,21 @@ def focused_stochastic_ricci_step(G, A, N, curvature_fn, target_curvature=0, giv
     else:
         ijs = [(i,j) for (i,j) in ijs if i < j]
     
-    candidate_adds = []
-    
     for i, j in ijs:
         for x in list(G.neighbors(i)) + [i]:
             for y in list(G.neighbors(j)) + [j]:
                 x_, y_ = (x,y) if x<y else (y,x)
                 if x_ != y_ and not A[x_,y_] and not (x_,y_) in candidates and mses[mapping[(x_,y_)]] > 0:
-                    candidates[(x_,y_)] = mses[mapping[(x_,y_)]]
+                        candidates[(x_,y_)] = mses[mapping[(x_,y_)]]
         if prioritise_betweenness and len(candidates) > 0:
             break
-    
+            
     return candidates
 
 
 STR_TO_CURVATURE_MAP = {
     'uma_forman': uma_forman_parallel,
+    'unbiased_forman': unbiased_forman_focused,
 }
 STR_TO_RICCI_STEP = {
     'fsdrf': focused_stochastic_ricci_step,
@@ -90,7 +177,10 @@ def stochastic_discrete_ricci_flow(
     ricci_step='fsdrf',
     max_steps=None
 ):
+    prioritise_betweenness = (prioritise_betweenness==True) or (prioritise_betweenness=="True")
+    consider_positivity = (consider_positivity==True) or (consider_positivity=="True")
     G = nx.from_numpy_matrix(A)
+    A = A.copy()
     added_edges = []
     removed_edges = []
 
@@ -114,7 +204,6 @@ def stochastic_discrete_ricci_flow(
         )
         if only_allow_positive_actions:
             scores = {k:v*scaling for k,v in scores.items() if v > 0}
-        # print(scores)
         scores_keys = list(scores.keys())
         scores_values = np.array(list(scores.values()))
         if np.any(scores_values > 0):
@@ -132,7 +221,6 @@ def stochastic_discrete_ricci_flow(
                 A[x,y] = A[y,x] = 0
                 removed_edges.append((x,y))
             else:
-                # print(f'Adding edge ({x},{y})')
                 G.add_edge(x,y)
                 A[x,y] = A[y,x] = 1
                 added_edges.append((x,y))
