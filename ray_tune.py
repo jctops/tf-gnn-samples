89a6bd916eff4889eafc92d46715e89d488493ec
Ben Chamberlain <bchamberlain@twitter.com>
2021-05-21 13:05:52 +0100

    adding tracking metric

--- ray_tune.py
+++ ray_tune.py
@@ -14,8 +14,8 @@ from test import test
 from run_GNN import setup
 from utils.model_utils import name_to_task_class, name_to_model_class
 
-def average_test(models, datas):
 
+def average_test(models, datas):
   results = [test(model, data) for model, data in zip(models, datas)]
   train_accs, val_accs, tmp_test_accs = [], [], []
 
@@ -26,9 +26,11 @@ def average_test(models, datas):
 
   return train_accs, val_accs, tmp_test_accs
 
+
 def set_search_space(opt):
   if opt["dataset"] == "qm9":
     return set_qm9_search_space(opt)
+
 
 def set_qm9_search_space(opt):
   """
@@ -39,10 +41,9 @@ def set_qm9_search_space(opt):
   opt['sdrf_curvature_fn'] = tune.choice(['unbiased_forman'])
   opt['sdrf_consider_positivity'] = tune.choice([True, False])
   opt['sdrf_target_curvature'] = tune.uniform(-0.5, 0.5)
-  opt['sdrf_scaling'] = tune.loguniform(5,100)
+  opt['sdrf_scaling'] = tune.loguniform(5, 100)
   # 'max_steps':[2,3,5,10]
   opt['sdrf_max_steps'] = tune.uniform(0.1, 0.5)
-
 
 
 def train_ray(opt, checkpoint_dir=None, data_dir="../data"):
@@ -58,20 +59,20 @@ def train_ray(opt, checkpoint_dir=None, data_dir="../data"):
   for epoch in range(1, opt["epoch"]):
     train_loss, train_task_metrics, train_num_graphs, train_graphs_p_s, train_nodes_p_s, train_edges_p_s = \
       model._Sparse_Graph_Model__run_epoch("epoch %i (training)" % epoch,
-                        model.task._loaded_data[DataFold.TRAIN],
-                        DataFold.TRAIN, quiet=False)
+                                           model.task._loaded_data[DataFold.TRAIN],
+                                           DataFold.TRAIN, quiet=False)
 
     print("\r\x1b[K", end='')
     model.log_line(" Train: loss: %.5f || %s || graphs/sec: %.2f | nodes/sec: %.0f | edges/sec: %.0f"
-                % (train_loss,
-                   model.task.pretty_print_epoch_task_metrics(train_task_metrics, train_num_graphs),
-                   train_graphs_p_s, train_nodes_p_s, train_edges_p_s))
+                   % (train_loss,
+                      model.task.pretty_print_epoch_task_metrics(train_task_metrics, train_num_graphs),
+                      train_graphs_p_s, train_nodes_p_s, train_edges_p_s))
 
     valid_loss, valid_task_metrics, valid_num_graphs, valid_graphs_p_s, valid_nodes_p_s, valid_edges_p_s = \
       model._Sparse_Graph_Model__run_epoch("epoch %i (validation)" % epoch,
-                       model.task._loaded_data[DataFold.VALIDATION],
-                       DataFold.VALIDATION,
-                       quiet=False)
+                                           model.task._loaded_data[DataFold.VALIDATION],
+                                           DataFold.VALIDATION,
+                                           quiet=False)
     tune.report(loss=train_loss, train_mae=train_task_metrics, val_mae=valid_task_metrics, train_nps=train_nodes_p_s,
                 val_nps=valid_nodes_p_s, train_gps=train_graphs_p_s, val_gps=valid_graphs_p_s)
 
@@ -127,11 +128,9 @@ if __name__ == "__main__":
     "--reduction_factor", type=int, default=10, help="number of trials is halved after this many epochs"
   )
   parser.add_argument("--name", type=str, default="ray_exp")
-  parser.add_argument("--metric", type=str, default="val_mae")
+  parser.add_argument("--metric", type=str, default="val_mae", help='metric to sort the hyperparameter tuning runs on')
   parser.add_argument("--num_splits", type=int, default=0, help="Number of random splits >= 0. 0 for planetoid split")
   parser.add_argument("--num_init", type=int, default=1, help="Number of random initializations >= 0")
-
-  parser.add_argument('--metric', type=str, default='accuracy', help='metric to sort the hyperparameter tuning runs on')
   args = parser.parse_args()
 
   opt = vars(args)
